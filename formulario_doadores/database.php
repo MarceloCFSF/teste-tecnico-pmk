3382e167b9447730e68d69b45abc74d170e7b10e
MarceloCFSF <marcelo.cfsf@gmail.com>
2025-02-01 17:33:16 -0300

    feat: configure database

---
+++ formulario_doadores/database.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'utils.php';
+
+loadEnv(__DIR__ . '/.env');
+
+class Database
+{
+  private PDO $pdo;
+
+  public function __construct()
+  {
+    $host = getenv('DB_HOST');
+    $port = getenv('DB_PORT');
+    $user = getenv('DB_USER');
+    $password = getenv('DB_PASS');
+    $dbname = getenv('DB_NAME');
+
+    try {
+      $dsn = "mysql:host=" . $host . ";dbname=" . $dbname . ";port=" . $port;
+      $this->pdo = new PDO($dsn, $user, $password);
+      $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+      throw new Exception("Erro na conexão: " . $e->getMessage());
+    }
+  }
+
+  public function getConnection(): PDO
+  {
+    return $this->pdo;
+  }
+}
