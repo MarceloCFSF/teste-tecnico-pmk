49d20222d5bb416c676ff61cea92cf778d72e09a
MarceloCFSF <marcelo.cfsf@gmail.com>
2025-02-03 22:14:44 -0300

    style: adjust form style

--- formulario_doadores/app/Views/donor/create.php
+++ formulario_doadores/app/Views/donor/create.php
@@ -12,6 +12,7 @@ $form = new FormHandler();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/assets/css/donor_form.css">
   <title>Cadastro de doadores</title>
 </head>
 
@@ -22,68 +23,93 @@ $form = new FormHandler();
     <fieldset>
       <legend>Dados pessoais</legend>
 
-      <label for="name">Nome</label>
-      <input id="name" name="name" value="<?= $form->old('name') ?>" type="text">
-      <?= $form->error('name') ?>
+      <div class="field">
+        <label for="name">Nome</label>
+        <input id="name" name="name" value="<?= $form->old('name') ?>" type="text">
+        <?= $form->error('name') ?>
+      </div>
 
-      <label for="email">Email</label>
-      <input id="email" name="email" value="<?= $form->old('email') ?>" type="email">
-      <?= $form->error('email') ?>
+      <div class="field">
+        <label for="email">Email</label>
+        <input id="email" name="email" value="<?= $form->old('email') ?>" type="email">
+        <?= $form->error('email') ?>
+      </div>
 
-      <label for="cpf">CPF</label>
-      <input id="cpf" name="cpf" value="<?= $form->old('cpf') ?>" type="text">
-      <?= $form->error('cpf') ?>
-
-      <label for="phone">Telefone</label>
-      <input id="phone" name="phone" value="<?= $form->old('phone') ?>" type="text">
-      <?= $form->error('phone') ?>
-
-      <label for="birthday">Data de Nascimento</label>
-      <input id="birthday" name="birthday" value="<?= $form->old('birthday') ?>" type="date">
-      <?= $form->error('birthday') ?>
+      <div class="flex-row">
+        <div class="field">
+          <label for="cpf">CPF</label>
+          <input id="cpf" name="cpf" value="<?= $form->old('cpf') ?>" type="text">
+          <?= $form->error('cpf') ?>
+        </div>
+  
+        <div class="field">
+          <label for="phone">Telefone</label>
+          <input id="phone" name="phone" value="<?= $form->old('phone') ?>" type="text">
+          <?= $form->error('phone') ?>
+        </div>
+  
+        <div class="field">
+          <label for="birthday">Data de Nascimento</label>
+          <input id="birthday" name="birthday" value="<?= $form->old('birthday') ?>" type="date">
+          <?= $form->error('birthday') ?>
+        </div>
+      </div>
     </fieldset>
 
     <fieldset>
       <legend>Doação</legend>
 
-      <label for="payment_interval">Intervalo de doação</label>
-      <select name="payment_interval" id="payment_interval">
-        <option value="">Selecione</option>
-        <option value="unique">Único</option>
-        <option value="bimonthly">Bimestral</option>
-        <option value="semi-annual">Semestral</option>
-        <option value="annual">anual</option>
-      </select>
-      <?= $form->error('payment_interval') ?>
+      <div style="display: flex; gap: 5em;">
+        <div class="field">
+          <label for="payment_interval">Intervalo de doação</label>
+          <select name="payment_interval" id="payment_interval">
+            <option value="">Selecione</option>
+            <option value="unique">Único</option>
+            <option value="bimonthly">Bimestral</option>
+            <option value="semi-annual">Semestral</option>
+            <option value="annual">anual</option>
+          </select>
+          <?= $form->error('payment_interval') ?>
+        </div>
+  
+        <div class="field">
+          <label for="value">Valor da doação</label>
+          <input id="value" name="value" type="text">
+          <?= $form->error('value') ?>
+        </div>
+      </div>
 
-      <label for="value">Valor da doação</label>
-      <input id="value" name="value" type="number">
-      <?= $form->error('value') ?>
+      <div class="field">
+          <label>Selecione a forma de pagamento</label>
+          
+          <label>
+            <input type="radio" id="credit" value="credit" name="payment_type" onchange="toggleInput()">Crédito
+          </label>
+  
+          <label for="debit">
+            <input type="radio" id="debit" value="debit" name="payment_type" onchange="toggleInput()">Débito
+          </label>
+        <?= $form->error('payment_type') ?>
+      </div>
 
-      <fieldset>
-        <legend>Selecione a forma de pagamento</legend>
-        <input type="radio" id="credit" value="credit" name="payment_type" onchange="toggleInput()">
-        <label for="credit">Crédito</label>
-
-        <input type="radio" id="debit" value="debit" name="payment_type" onchange="toggleInput()">
-        <label for="debit">Débito</label>
-      </fieldset>
-      <?= $form->error('payment_type') ?>
-      
-      <div id="debit_field" style="display: none;">
+      <div id="debit_field" class="field" style="display: none;">
         <label for="account">Número da Conta</label>
         <input id="account" name="account" type="number">
         <?= $form->error('account') ?>
       </div>
 
       <div id="credit_field" style="display: none;">
-        <label for="first_card_numbers">Primeiros números do cartão</label>
-        <input id="first_card_numbers" name="first_card_numbers" type="number">
-        <?= $form->error('first_card_numbers') ?>
-        
-        <label for="last_card_numbers">Últimos números do cartão</label>
-        <input id="last_card_numbers" name="last_card_numbers" type="number">
-        <?= $form->error('last_card_numbers') ?>
+        <div class="field">
+          <label for="first_card_numbers">Primeiros números do cartão</label>
+          <input id="first_card_numbers" name="first_card_numbers" type="number">
+          <?= $form->error('first_card_numbers') ?>
+        </div>
+
+        <div class="field">
+          <label for="last_card_numbers">Últimos números do cartão</label>
+          <input id="last_card_numbers" name="last_card_numbers" type="number">
+          <?= $form->error('last_card_numbers') ?>
+        </div>
       </div>
       <?= $form->error('credit_card') ?>
     </fieldset>
@@ -91,25 +117,40 @@ $form = new FormHandler();
     <fieldset>
       <legend>Endereço</legend>
 
-      <label for="street">Rua / Avenida</label>
-      <input id="street" name="street" value="<?= $form->old('street') ?>" type="text">
-      <?= $form->error('street') ?>
+      <div class="address-fields">
+        <div class="field" style="width: 100%;">
+          <label for="street">Rua / Avenida</label>
+          <input id="street" name="street" value="<?= $form->old('street') ?>" type="text">
+          <?= $form->error('street') ?>
+        </div>
+  
+        <div class="field">
+          <label for="address_number">Número</label>
+          <input id="address_number" name="address_number" value="<?= $form->old('address_number') ?>" type="number">
+          <?= $form->error('address_number') ?>
+        </div>
+      </div>
 
-      <label for="address_number">Número</label>
-      <input id="address_number" name="address_number" value="<?= $form->old('address_number') ?>" type="number">
-      <?= $form->error('address_number') ?>
+      <div class="flex-row">
+        <div class="field">
+          <label for="neighborhood">Bairro</label>
+          <input id="neighborhood" name="neighborhood" value="<?= $form->old('neighborhood') ?>" type="text">
+          <?= $form->error('neighborhood') ?>
+        </div>
 
-      <label for="neighborhood">Bairro</label>
-      <input id="neighborhood" name="neighborhood" value="<?= $form->old('neighborhood') ?>" type="text">
-      <?= $form->error('neighborhood') ?>
+        <div class="field">
+          <label for="state">Estado</label>
+          <input id="state" name="state" value="<?= $form->old('state') ?>" type="text">
+          <?= $form->error('state') ?>
+        </div>
 
-      <label for="state">Estado</label>
-      <input id="state" name="state" value="<?= $form->old('state') ?>" type="text">
-      <?= $form->error('state') ?>
+        <div class="field">
+          <label for="city">Cidade</label>
+          <input id="city" name="city" value="<?= $form->old('city') ?>" type="text">
+          <?= $form->error('city') ?>
+        </div>
+      </div>
 
-      <label for="city">Cidade</label>
-      <input id="city" name="city" value="<?= $form->old('city') ?>" type="text">
-      <?= $form->error('city') ?>
     </fieldset>
 
     <button type="submit">Enviar</button>
